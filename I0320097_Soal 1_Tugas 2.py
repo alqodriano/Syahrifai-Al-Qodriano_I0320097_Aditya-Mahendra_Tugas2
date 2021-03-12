9a699bf259a28a3ab24daeb4e37fa7a4f8a28fbb
alqodriano <80144761+alqodriano@users.noreply.github.com>
2021-03-12 15:57:44 +0700

    Tugas 2 Praktek Programa Komputer

---
+++ I0320097_Soal 1_Tugas 2.py
@@ -0,0 +1,27 @@
+#Soal 1
+#Menghitung luas persegi panjang
+Panjang = 4
+Lebar = 2
+Luas_persegi_panjang = Panjang * Lebar
+print('Luas persegi panjang : ', Luas_persegi_panjang)
+
+#Menghitung luas lingkaran
+r = 14
+Luas_lingkaran = 3.14 *(r*r)
+print('Luas lingkaran : ', Luas_lingkaran)
+
+#Menghitung luas kubus
+Panjang_rusuk = 4
+sisi = 6
+Luas_Kubus = sisi * (Panjang_rusuk ** 2)
+print('Luas Kubus : ', Luas_Kubus)
+
+#Menghitung konversi suhu celcius ke fahrenheit
+celcius = 44
+fahrenheit = ((9/5) * 10) + 32
+print("Suhu 44 Celcius : ", fahrenheit, "fahrenheit",)
+
+#Menghitung konversi suhu reamur ke kelvin
+reamur = 14
+kelvin = ((5/4) * 20) + 273
+print("Suhu 14 Reamur :", kelvin, "Kelvin" )
---
+++ I0320097_Soal2_Tugas2.py
@@ -0,0 +1,40 @@
+#Soal 2
+
+nama_lengkap = "Syahrifai Al Qodriano"
+nama_panggilan = "ano"
+umur = 18
+jenis_kelamin = "laki laki"
+anak_ke = 2
+agama = "islam"
+golongan_darah = "O"
+Tinggi_badan = 170.5
+Berat_badan = 70
+ukuran_sepatu = 42.5
+hobi = "berenang"
+gaya = "dada,punggung,bebas,dan kupu kupu"
+Kewarganegaraan = "Indonesia"
+Tempat_lahir = "Malang"
+Tanggal_lahir = "7 Oktober 2002"
+Alamat = "Jalan Jeblogan,Ceper,Klaten"
+cita_cita = "Pengusaha"
+Jurusan = "Teknik Industri"
+Perguruan_tinggi = "Universitas Sebelas Maret Surakarta"
+
+print("nama saya", nama_lengkap, "biasa dipanggil", nama_panggilan)
+print("saya berkewarganegaraan", Kewarganegaraan)
+print("saya bertempat tinggal di", Alamat)
+print("saya lahir di kota", Tempat_lahir, "pada tanggal", Tanggal_lahir)
+print("umur saya", umur)
+print("jenis kelamin saya",jenis_kelamin)
+print("Dari 3 bersaudara saya anak ke", anak_ke)
+print("Saya beragama", agama)
+print("golongan darah saya", golongan_darah)
+print("tinggi badan saya", Tinggi_badan, "berat badan saya", Berat_badan,"ukuran sepatu saya", ukuran_sepatu)
+print("hobi saya", hobi,"saya sudah menguasai semua gaya yaitu gaya", gaya)
+print("saya bercita cita menjadi seorang", cita_cita)
+print("oleh karena itu saya kuliah di jurusan", Jurusan,Perguruan_tinggi)
+
+
+
+
+
